2913fcfaebf899385314c0277158dabfa204eece
ceceliacreates <ceceliacreates@gmail.com>
2020-11-13 22:29:57 -0500

    ionic build works and connects to Netlify function for API call.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,45 +2,82 @@
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+        <ion-title>BallotCure</ion-title>
       </ion-toolbar>
     </ion-header>
-    
+
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
+          <ion-title size="large">BallotCure</ion-title>
         </ion-toolbar>
       </ion-header>
-    
+
       <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+        <p>{{ ballot.voterFirstName }} {{ ballot.voterLastName }}</p>
+        <p>{{ ballot.issueDescription }}</p>
       </div>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
-import { defineComponent } from 'vue';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/vue";
+import { defineComponent, onMounted, ref } from "vue";
+
+import "@capacitor-community/http";
+
+import { Plugins } from "@capacitor/core";
+
+async function fetchBallot(ballotId: string) {
+  const { Http } = Plugins;
+
+  const response = await Http.request({
+    method: "GET",
+    url: `https://hungry-brown-da828c.netlify.app/.netlify/functions/server/ballots/${ballotId}`,
+  });
+
+  const ballot = response.data;
+
+  return ballot;
+}
 
 export default defineComponent({
-  name: 'Home',
+  name: "Home",
   components: {
     IonContent,
     IonHeader,
     IonPage,
     IonTitle,
-    IonToolbar
-  }
+    IonToolbar,
+  },
+  setup() {
+    const ballot = ref({});
+    const ballotId = ref("12345");
+    const getBallot = async () => {
+      ballot.value = await fetchBallot(ballotId.value);
+    };
+
+    onMounted(getBallot);
+
+    return {
+      ballot,
+      getBallot,
+    };
+  },
 });
 </script>
 
 <style scoped>
 #container {
   text-align: center;
-  
+
   position: absolute;
   left: 0;
   right: 0;
@@ -56,9 +93,9 @@ export default defineComponent({
 #container p {
   font-size: 16px;
   line-height: 22px;
-  
+
   color: #8c8c8c;
-  
+
   margin: 0;
 }
 
